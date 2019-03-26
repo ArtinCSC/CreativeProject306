89c4a60ba742eef38e9d3b71c5886c44c2e9bd04
ArtinCSC <amalekia@masonlive.gmu.edu>
2019-03-25 20:13:14 -0400

    .

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
 
- //the correct one
-var slideIndex = 0;
-var t;
-var s = 1;
-
-
-// function navbarFunction() {
-//     var x = document.getElementById("myNavbar");
-//     var y = document.getElementById("iconn");
-//     if (x.className === "navbar") {
-//         x.className += " responsive";
-//         //y.innerHTML = "&#xf2d1;";//"&#xf00d;";
-//       //  openNav();
-//       //  document.getElementById("slides").style.zIndex = "";
-//         y.className = "fa fa-window-minimize";
-//     } else {
-//         x.className = "navbar";
-//        // document.getElementById("slides").style.zIndex = "9";
-//        // y.innerHTML = "&#9776;";
-//         y.className = "fa fa-navicon";
-//     }
-// }
-
-
     $(document).ready(function(){
         // Add scrollspy to <body>
         $('body').scrollspy({ target: ".navbar", offset: 50 });
@@ -41,7 +17,7 @@ var s = 1;
     // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 1000, function(){
+      }, 2000, function(){
 
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
@@ -49,13 +25,3 @@ var s = 1;
   }  // End if
 });
 });
-
-//
-//function openNav() {
-//    document.getElementById("myNavbar").style.width = "250px";
-//}
-//
-//function closeNav() {
-//    document.getElementById("myNavbar").style.width = "0";
-//}
-
