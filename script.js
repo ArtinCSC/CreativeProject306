6be887a0dc0f217a03427fa5b62555d1d5845cc0
ArtinCSC <amalekia@masonlive.gmu.edu>
2019-06-30 21:04:29 -0400

    html files was edited.

--- script.js
+++ script.js
@@ -1,39 +1,38 @@
-
 $(document).ready(function () {
-  // Add scrollspy to <body>
-  $('body').scrollspy({ target: ".navbar", offset: 50 });
+    // Add scrollspy to <body>
+    $('body').scrollspy({target: ".navbar", offset: 50});
 
-  // Add smooth scrolling on all links inside the navbar
-  $("#myNavbar a").on('click', function (event) {
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+    // Add smooth scrolling on all links inside the navbar
+    $("#myNavbar a").on('click', function (event) {
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+            // Prevent default anchor click behavior
+            event.preventDefault();
 
-      // Store hash
-      var hash = this.hash;
+            // Store hash
+            var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 1500, function () {
+            // Using jQuery's animate() method to add smooth page scroll
+            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1500, function () {
 
-        // Add hash (#) to URL when done scrolling \
-        window.location.hash = hash;
-      });
-    }  // End if
-  });
+                // Add hash (#) to URL when done scrolling \
+                window.location.hash = hash;
+            });
+        }  // End if
+    });
 });
 
 // Accordion 
 function myAccFunc() {
-  var x = document.getElementById("demoAcc");
-  if (x.className.indexOf("w3-show") == -1) {
-    x.className += " w3-show";
-  } else {
-    x.className = x.className.replace(" w3-show", "");
-  }
+    var x = document.getElementById("demoAcc");
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
 }
 
 // Click on the "Jeans" link on page load to open the accordion for demo purposes
@@ -41,13 +40,13 @@ document.getElementById("myBtn").click();
 
 // Open and close sidebar
 function w3_open() {
-  document.getElementById("mySidebar").style.display = "block";
-  document.getElementById("myOverlay").style.display = "block";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("myOverlay").style.display = "block";
 }
 
 function w3_close() {
-  document.getElementById("mySidebar").style.display = "none";
-  document.getElementById("myOverlay").style.display = "none";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("myOverlay").style.display = "none";
 }
 
 
